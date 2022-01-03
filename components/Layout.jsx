43db6a80fccf84f0d6e2b488056b54decd67d203
bushbass <alex.nielsen@gmail.com>
2022-01-03 18:18:22 -0500

    added Link tag to hamburger

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,19 +1,26 @@
 import Header from './Header'
 import Footer from './Footer'
 import { slide as Menu } from 'react-burger-menu'
+import Link from 'next/link'
 function Layout({ children }) {
   return (
     <div>
       <Menu pageWrapId={'page-wrap'}>
-        <a id='home' className='menu-item' href='/'>
-          Home
-        </a>
-        <a id='about' className='menu-item' href='/about'>
-          About
-        </a>
-        <a id='contact' className='menu-item' href='/contact'>
-          Contact
-        </a>
+        <Link href='/'>
+          <a id='home' className='menu-item'>
+            Home
+          </a>
+        </Link>
+        <Link href='/about'>
+          <a id='about' className='menu-item'>
+            About
+          </a>
+        </Link>
+        <Link href='/contact'>
+          <a id='contact' className='menu-item'>
+            Contact
+          </a>
+        </Link>
       </Menu>
       <main id='page-wrap'>
         <>
