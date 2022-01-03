e4743ca3cc4494b7271693b3a5fc134586fc2d99
bushbass <alex.nielsen@gmail.com>
2022-01-03 18:21:19 -0500

    fixed another missing Link

--- components/Hamburger.jsx
+++ components/Hamburger.jsx
@@ -8,20 +8,21 @@ class Example extends React.Component {
   render() {
     // NOTE: You also need to provide styles, see https://github.com/negomi/react-burger-menu#styling
     return (
-      <Menu>
-        <a id='home' className='menu-item' href='/'>
-          Home
-        </a>
-        <a id='about' className='menu-item' href='/about'>
-          About
-        </a>
-        <a id='contact' className='menu-item' href='/contact'>
-          Contact
-        </a>
-        <a onClick={this.showSettings} className='menu-item--small' href=''>
-          Settings
-        </a>
-      </Menu>
+      <div>hi</div>
+      //   <Menu>
+      //     <a id='home' className='menu-item' href='/'>
+      //       Home
+      //     </a>
+      //     <a id='about' className='menu-item' href='/about'>
+      //       About
+      //     </a>
+      //     <a id='contact' className='menu-item' href='/contact'>
+      //       Contact
+      //     </a>
+      //     <a onClick={this.showSettings} className='menu-item--small' href=''>
+      //       Settings
+      //     </a>
+      //   </Menu>
     )
   }
 }
