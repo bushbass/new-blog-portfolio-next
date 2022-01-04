290fe32448f4475becbdf689477f26e673ccbbb7
Alex Nielsen <alex@bigal.org>
2022-01-04 13:58:20 -0500

    just didnt save all open pages on last commit

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Alex Nielsen </h1>{' '}
+        <h1 className={styles.title}>Alex Nielsen </h1>
         <div className="image-wrapper">
           <Image
             src="/images/me-wearing-hat.jpg"
