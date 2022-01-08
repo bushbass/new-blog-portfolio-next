57a3a35ffc120015b6b353ee251010e8b66c0ffc
Alex Nielsen <alex@bigal.org>
2022-01-08 16:37:02 -0500

    added Image to social icons

--- components/SocialIcons.jsx
+++ components/SocialIcons.jsx
@@ -1,29 +1,39 @@
+import Image from 'next/image';
+
 function SocialIcons() {
   return (
     <div className="social-image-container">
       <a href="http://AlexNielsen.com/resume">
-        <img
+        <Image
+          width={40}
+          height={40}
           className="social-image"
           src={`/images/social/gmail.png`}
           alt="gmail logo"
         />
       </a>
       <a href="https://linkedin.com/in/alex-nielsen-nj">
-        <img
+        <Image
+          width={40}
+          height={40}
           className="social-image"
           src={`/images/social/linkedin.png`}
           alt="linkedin logo"
         />
       </a>
       <a href="https://twitter.com/BigAlsHouse">
-        <img
+        <Image
+          width={40}
+          height={40}
           className="social-image"
           src={`/images/social/twitter.png`}
           alt="twitter logo"
         />
       </a>
       <a href="https://www.youtube.com/channel/UCqdNQHOLz3PmB4cDvQkdQ1w/">
-        <img
+        <Image
+          width={40}
+          height={40}
           className="social-image"
           src={`/images/social/youtube.png`}
           alt="youtube logo"
