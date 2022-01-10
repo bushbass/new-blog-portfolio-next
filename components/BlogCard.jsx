24d6dba60cb1130b14482b5fda40471296ca191e
Alex Nielsen <alex@bigal.org>
2022-01-10 11:54:13 -0500

    uppercase N in className in BlogCard fixed problem

--- components/BlogCard.jsx
+++ components/BlogCard.jsx
@@ -1,13 +1,13 @@
-import Link from 'next/link'
-import styles from '../styles/Home.module.css'
+import Link from 'next/link';
+import styles from '../styles/Home.module.css';
 function BlogCard({ post }) {
   return (
     <Link href={`/blog/${post.slug}`}>
-      <a classname={styles.card}>
+      <a className={styles.card}>
         <h2>{post.frontmatter.title} &rarr;</h2>
       </a>
     </Link>
-  )
+  );
 }
 
-export default BlogCard
+export default BlogCard;
