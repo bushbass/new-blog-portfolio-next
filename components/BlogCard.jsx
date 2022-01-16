a30178be3183da143498b5f5882c468bbcca9c84
bushbass <alex.nielsen@gmail.com>
2022-01-16 15:18:38 -0500

    fixed blog and project routing problem

--- components/BlogCard.jsx
+++ components/BlogCard.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 function BlogCard({ post }) {
   return (
-    <Link href={`/projects/${post.projectName}`}>
+    <Link href={`/blog/${post.slug}`}>
       <a className={styles.card}>
         <h2>{post.frontmatter.title} &rarr;</h2>
         <p>{post.frontmatter.date}</p>
