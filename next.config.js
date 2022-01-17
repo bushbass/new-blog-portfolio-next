017066edc5dfce6a0bf86d9cf0df1248da28f044
Alex Nielsen <alex.nielsen@gmail.com>
2022-01-17 10:27:40 -0500

    added redirect for resume in next.config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
-module.exports = {
-  reactStrictMode: true,
+module.exports = { reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/resume',
+        destination: 'https://docs.google.com/document/d/1OR_FV2JG7cnFN5UvEg6kjDb-0JTfI1QAdKwFYxQqjkU/edit',
+        permanent: true,
+      },
+    ]
+  },
 }
