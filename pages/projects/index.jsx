2ba225358bdc63f67a744b4e31b574e3384e73da
Alex Nielsen <alex@bigal.org>
2023-01-10 11:42:22 -0500

    changed header on projects from blog to projects

--- pages/projects/index.jsx
+++ pages/projects/index.jsx
@@ -1,16 +1,16 @@
-import fs from 'fs'
-import path from 'path'
-import matter from 'gray-matter'
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
 // import Post from "../components/Post";
-import { sortByDate } from '../../utils'
-import styles from '../../styles/Home.module.css'
-import ProjectCard from '../../components/ProjectCard'
+import { sortByDate } from '../../utils';
+import styles from '../../styles/Home.module.css';
+import ProjectCard from '../../components/ProjectCard';
 function Project({ projects }) {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1 className={styles.title}>Web Development Blog</h1>
-        <div className='grid'>
+        <h1 className={styles.title}>Web Development Projects</h1>
+        <div className="grid">
           {console.log({ projects })}
           <div className={styles.grid}>
             {projects.map((post) => (
@@ -20,10 +20,10 @@ function Project({ projects }) {
         </div>
       </main>
     </div>
-  )
+  );
 }
 
-export default Project
+export default Project;
 
 // export default function Home({ projects }) {
 //   // this 'projects' prop comes from the getStaticProps function below
@@ -39,34 +39,34 @@ export async function getStaticProps() {
   // returns array of filenames
   const dirents = fs.readdirSync(path.join('projects'), {
     withFileTypes: true,
-  })
+  });
   const filesNames = dirents
     .filter((dirent) => dirent.isFile())
-    .map((dirent) => dirent.name)
+    .map((dirent) => dirent.name);
   // get projectName and frontmatter from markdown files
   // maps over the array of filenames returned in the last step
 
   const projects = filesNames.map((filename) => {
     // create projectName by removing extension from filename
-    const projectName = filename.replace('.md', '')
+    const projectName = filename.replace('.md', '');
 
     // get frontmatter from each file
     const markdownWithMeta = fs.readFileSync(
       path.join('projects', filename),
       'utf-8'
-    )
+    );
 
     // use gray-matter to convert the markdown/frontmatter
-    const { data: frontmatter } = matter(markdownWithMeta)
+    const { data: frontmatter } = matter(markdownWithMeta);
     // return projects array that has each post's projectName and frontmatter
-    return { projectName, frontmatter }
-  })
+    return { projectName, frontmatter };
+  });
 
   // now send that projects array as a prop to be used in the page
   return {
     props: {
       projects: projects.sort(sortByDate),
     },
-  }
+  };
   // btw, this whole thing could be from an API or something instead of .md files
 }
