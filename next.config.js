18448ddae69a2f932458d30e0fa28f04759acbda
Alex Nielsen <alex@bigal.org>
2023-03-03 12:11:05 -0500

    updated /resume link

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
-module.exports = { reactStrictMode: true,
+module.exports = {
+  reactStrictMode: true,
   async redirects() {
     return [
       {
         source: '/resume',
-        destination: 'https://docs.google.com/document/d/13KlhMzzbGxa-U8yt1zE9qdAyGklGH1P8CJlj15OioxQ/edit?usp=sharing',
+        destination:
+          'https://docs.google.com/document/d/1w1tsK-C9ysDKSwaVT_szGmsc4ST6WZjqARZrJ9r0c9Q/edit?usp=sharing',
         permanent: true,
       },
-    ]
+    ];
   },
-}
+};
