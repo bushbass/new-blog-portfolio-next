40eb280af905c1c474e2617965fca8410d281561
Alex Nielsen <alex@bigal.org>
2023-03-03 12:22:57 -0500

    removed date from project list cards

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -1,14 +1,15 @@
-import Link from 'next/link'
-import styles from '../styles/Home.module.css'
+import Link from 'next/link';
+import styles from '../styles/Home.module.css';
 function ProjectCard({ post }) {
   return (
     <Link href={`/projects/${post.projectName}`}>
       <a className={styles.card}>
         <h2>{post.frontmatter.title} &rarr;</h2>
-        <p>{post.frontmatter.date}</p>
+        <p>{post.frontmatter.appType}</p>
+        {console.log(post)}
       </a>
     </Link>
-  )
+  );
 }
 
-export default ProjectCard
+export default ProjectCard;
