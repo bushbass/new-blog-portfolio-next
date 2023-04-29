16bdd00b8891ad8329fc12497879eca69e87dc6d
Alex Nielsen <alex.nielsen@gmail.com>
2023-04-29 11:36:29 -0400

    added analytics from vercel

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
-import '../styles/globals.css'
-import Layout from '../components/Layout'
+import '../styles/globals.css';
+import Layout from '../components/Layout';
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
